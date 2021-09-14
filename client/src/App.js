00a529c76645301181e97d39540d01307e402db5
tgavrel <tomas.gavrel@hotmail.fr>
2021-09-14 10:15:42 +0200

    Installed React-Bootstrap :package:

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,32 @@
 // React imports
 import { useEffect, useState } from "react";
 
-import './App.css';
+//CSS imports
+import "./App.css";
 
 function App() {
   const [recettes, setRecettes] = useState(null);
 
-  useEffect(()=>{
-    fetch('http://localhost:9000/api/recipes')
-      .then(res => res.json())
-      .then(recipes => {
+  useEffect(() => {
+    fetch("http://localhost:9000/api/recipes")
+      .then((res) => res.json())
+      .then((recipes) => {
         setRecettes(recipes);
-      })
-  }, [])
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1>Liste des recettes</h1>
-       {recettes && recettes.map(recette =>
-        <div key={recette.id}>
-          <h2>{recette.titre}</h2>
-          <p>{recette.description}</p>
-        </div>
-        )
-      }
+      {recettes &&
+        recettes.map((recette) => (
+          <div key={recette.id}>
+            <h2>{recette.titre}</h2>
+            <p>{recette.description}</p>
+          </div>
+        ))}
     </div>
-  )
+  );
 }
 
 export default App;
