c083068f9ae8bfa200f59548d267c8a44fda77ed
tgavrel <tomas.gavrel@hotmail.fr>
2021-09-14 11:49:35 +0200

    Configured Home and Recipe components :wrench:

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,24 @@ import { useEffect, useState } from "react";
 //CSS imports
 import "./App.css";
 
+// Router-dom imports
+import {
+  BrowserRouter,
+  Route,
+  NavLink,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
+// Bootstrap imports
+import "bootstrap/dist/css/bootstrap.css";
+import { Navbar } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+
+// Views imports
+import Home from "./views/Home";
+import Recipe from "./views/Recipe";
+
 function App() {
   const [recettes, setRecettes] = useState(null);
 
@@ -20,10 +38,39 @@ function App() {
       <div className="App">
         <Container>
           <header className="mb-5">
-           
+            <Navbar bg="dark" variant="dark">
+              <NavLink
+                className="nav-link"
+                activeStyle={{ color: "orange" }}
+                to="/"
+                exact
+              >
+                Home
+              </NavLink>
+              <NavLink
+                className="nav-link"
+                activeStyle={{ color: "orange" }}
+                to="/recherche"
+                exact
+              >
+                Liste des recettes
+              </NavLink>
+            </Navbar>
           </header>
 
+          {/* Home */}
+          <Switch>
+            <Route path="/" component={Home} exact>
+              <Home></Home>
+            </Route>
 
+            {/* Recipe */}
+            <Route path="/recipe/:id" component={Recipe} exact>
+              <Recipe></Recipe>
+            </Route>
+
+            <Redirect to="/" />
+          </Switch>
         </Container>
       </div>
     </BrowserRouter>
